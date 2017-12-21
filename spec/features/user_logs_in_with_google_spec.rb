bb3b728592ae9f9ec353deee794f76ec9be518ff
Alan Lee <alanjlee89@gmail.com>
2017-12-21 17:01:24 -0500

    rspec test fix

--- spec/features/user_logs_in_with_google_spec.rb
+++ spec/features/user_logs_in_with_google_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.feature "user logs in" do
-  scenario "using google oauth2" do
+RSpec.feature 'user logs in' do
+  scenario 'using google oauth2' do
     stub_omniauth
     visit root_path
-    expect(page).to have_link("Sign in with Google")
-    click_link "Sign in with Google"
-    expect(page).to have_content("Jesse Spevack")
-    expect(page).to have_link("Logout")
+    expect(page).to have_link('Sign in with Google')
+    click_link 'Sign in with Google'
+    expect(page).to have_content('Jesse Spevack')
+    expect(page).to have_link('Logout')
   end
 end
 
@@ -17,16 +17,16 @@ def stub_omniauth
   # then, provide a set of fake oauth data that
   # omniauth will use when a user tries to authenticate:
   OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({
-      provider: "google",
-      uid: "12345678910",
+      provider: 'google',
+      uid: '12345678910',
       info: {
-        email: "jesse@mountainmantechnologies.com",
-        first_name: "Jesse",
-        last_name: "Spevack"
+        email: 'jesse@mountainmantechnologies.com',
+        first_name: 'Jesse',
+        last_name: 'Spevack'
       },
       credentials: {
-        token: "abcdefg12345",
-        refresh_token: "12345abcdefg",
+        token: 'abcdefg12345',
+        refresh_token: '12345abcdefg',
         expires_at: DateTime.now,
       }
   })
