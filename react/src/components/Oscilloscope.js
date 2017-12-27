88e63d4d327637008203e241224cc38d4e4d684c
Alan Lee <alanjlee89@gmail.com>
2017-12-27 16:13:58 -0500

    fixed react tests

--- react/src/components/Oscilloscope.js
+++ react/src/components/Oscilloscope.js
@@ -8,47 +8,44 @@ if (document.getElementById('scope')) {
   var scopeCtx = document.getElementById('scope').getContext('2d');
   draw();
 }
-  function draw() {
-    if (analyser){
-    drawScope(analyser, scopeCtx);
-    requestAnimationFrame(draw);
-    }
+
+function draw() {
+  if (analyser){
+  drawScope(analyser, scopeCtx);
+  requestAnimationFrame(draw);
   }
+}
 
-  function drawScope(analyser, ctx) {
-    var width = ctx.canvas.width;
-    var height = ctx.canvas.height;
-    var timeData = new Uint8Array(analyser.frequencyBinCount);
-    var scaling = height / 256;
-    var risingEdge = 0;
-    var edgeThreshold = 5;
+function drawScope(analyser, ctx) {
+  var width = ctx.canvas.width;
+  var height = ctx.canvas.height;
+  var timeData = new Uint8Array(analyser.frequencyBinCount);
+  var scaling = height / 256;
+  var risingEdge = 0;
+  var edgeThreshold = 5;
 
-    analyser.getByteTimeDomainData(timeData);
+  analyser.getByteTimeDomainData(timeData);
 
-    ctx.fillStyle = 'rgba(0, 20, 0, 0.1)';
-    ctx.fillRect(0, 0, width, height);
+  ctx.fillStyle = 'rgba(0, 20, 0, 0.1)';
+  ctx.fillRect(0, 0, width, height);
 
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = 'rgb(200, 0, 0)';
-    ctx.beginPath();
+  ctx.lineWidth = 2;
+  ctx.strokeStyle = 'rgb(200, 0, 0)';
+  ctx.beginPath();
 
-    // No buffer overrun protection
-    while (timeData[risingEdge++] - 128 > 0 && risingEdge <= width);
-    if (risingEdge >= width) risingEdge = 0;
+  // No buffer overrun protection
+  while (timeData[risingEdge++] - 128 > 0 && risingEdge <= width);
+  if (risingEdge >= width) risingEdge = 0;
 
-    while (timeData[risingEdge++] - 128 < edgeThreshold && risingEdge <= width);
-    if (risingEdge >= width) risingEdge = 0;
+  while (timeData[risingEdge++] - 128 < edgeThreshold && risingEdge <= width);
+  if (risingEdge >= width) risingEdge = 0;
 
-    for (var x = risingEdge; x < timeData.length && x - risingEdge < width; x++)
-    ctx.lineTo(x - risingEdge, height - timeData[x] * scaling);
+  for (var x = risingEdge; x < timeData.length && x - risingEdge < width; x++)
+  ctx.lineTo(x - risingEdge, height - timeData[x] * scaling);
 
-    ctx.stroke();
-  }
-
-  return(
-    <div>
-    </div>
-  )
+  ctx.stroke();
+}
+  return()
 }
 
 export default Oscilloscope;
